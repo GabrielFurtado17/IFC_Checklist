64f2290f1a4bbe58902f3690c77f4ec4df2e7441
GabrielFurtado17 <43618559+GabrielFurtado17@users.noreply.github.com>
2018-11-29 14:40:40 -0200

    Update structure_dump.sql

--- structure_dump.sql
+++ structure_dump.sql
@@ -1,34 +1,34 @@
 CREATE TABLE usuario (
-    id_usuario int NOT NULL AUTO_INCREMENT,
-    nome varchar (150) NOT NULL,
-    email varchar(150) NOT NULL,
-    senha varchar(40) NOT NULL,
-    dt_nasc date NOT NULL,
-    primary key (id_usuario)
+    id_usuario INTEGER NOT NULL AUTO_INCREMENT,
+    nome VARCHAR (150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    senha VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id_usuario)
 );
 CREATE TABLE projeto (
-    id_projeto int NOT NULL AUTO_INCREMENT,
-    NomeDoProjeto varchar (150) NOT NULL,
-    Descricao varchar (150) NOT NULL,
-    FK_Adm int NOT NULL,
-    primary key(id_projeto),
-    FOREIGN KEY (FK_Adm) REFERENCES usuario(id_usuario)
+    id_projeto INTEGER NOT NULL AUTO_INCREMENT,
+    nomedoprojeto VARCHAR (150) NOT NULL,
+    descricao VARCHAR (150) NOT NULL,
+    id_usuario INTEGER NOT NULL,
+    PRIMARY KEY(id_projeto),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 CREATE TABLE foto (
-    id_foto int NOT NULL AUTO_INCREMENT,
-    Url varchar(255) NOT NULL,
-    Descricao varchar(150) NOT NULL,
-    primary key (id_foto)
+    id_foto INTEGER NOT NULL AUTO_INCREMENT,
+    url VARCHAR(255) NOT NULL,
+    descricao VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id_foto)
 );
 CREATE TABLE tarefa (
-    id_tarefa int NOT NULL AUTO_INCREMENT,
-    NomeDaTarefa varchar (150) NOT NULL,
-    Descricao varchar (150) NOT NULL,
-    FK_Pjt int,
-    FK_Responsavel int NOT NULL,
-    FK_Foto int NOT NULL,
-    primary key(id_tarefa),
-    FOREIGN KEY (FK_Pjt) REFERENCES projeto(id_projeto),
-    FOREIGN KEY (FK_Responsavel) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (FK_Foto) REFERENCES foto(id_foto)
+    id_tarefa INTEGER NOT NULL AUTO_INCREMENT,
+    nome VARCHAR (150) NOT NULL,
+    descricao VARCHAR (150) NOT NULL,
+    estado INTEGER NOT NULL,
+    id_projeto INTEGER,
+    id_usuario INTEGER NOT NULL,
+    id_foto INTEGER NOT NULL,
+    PRIMARY KEY(id_tarefa),
+    FOREIGN KEY (id_projeto) REFERENCES projeto(id_projeto),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_foto) REFERENCES foto(id_foto)
 );
